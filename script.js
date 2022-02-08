f2032a2fcec44229e833d73346210017f9a46325
alternadiva <asomeonelse@gmail.com>
2022-02-08 21:55:32 +0100

    render last race data to html

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", fetchLastRace)
+let previousRace = document.getElementById("previous");
+
+window.addEventListener("load", fetchLastRace);
 
 function fetchLastRace() {
     fetch("http://ergast.com/api/f1/current/last.json")
@@ -11,6 +13,22 @@ function fetchLastRace() {
                 return response.json();
             }
         })
-        .then((data) => console.log(data))
+        .then((data) => {
+            let previousTitle = "<h2>Last Race</h2>";
+            let raceName = data["MRData"]["RaceTable"]["Races"][0]["raceName"];
+            let raceCircuit = data["MRData"]["RaceTable"]["Races"][0]["Circuit"]["circuitName"];
+            let raceDate = data["MRData"]["RaceTable"]["Races"][0]["date"];
+            let raceStart = data["MRData"]["RaceTable"]["Races"][0]["time"];
+            previousTitle += `
+                <p>${raceName}</p>
+                <p>${raceCircuit}</p>
+                <p>${raceDate}</p>
+                <p>${raceStart}</p>
+            `;
+            previousRace.innerHTML = previousTitle;
+            console.log(data["MRData"]["RaceTable"]["Races"][0])
+        })
         .catch((error) => console.log(error));
 }
+
+// Refine with promise all !!!
