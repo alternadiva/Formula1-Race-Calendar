57e7ad1e0c1807a0c4dd938a3b77df009c441b2e
alternadiva <asomeonelse@gmail.com>
2022-02-08 21:18:47 +0100

    throw error when end point not accesible

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ window.addEventListener("load", fetchLastRace)
 
 function fetchLastRace() {
     fetch("http://ergast.com/api/f1/current/last.json")
-        .then((response) => response.json())
+        .then((response) => {
+            if (!response.ok) {
+                const error = new Error(response.status);
+                throw error;
+            }
+            else {
+                return response.json();
+            }
+        })
         .then((data) => console.log(data))
-        .catch((error) => console.log("Error Reading data " + error));
+        .catch((error) => console.log(error));
 }
