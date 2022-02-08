4775b82c292353e7187a05ab6c2789370ac4119b
alternadiva <asomeonelse@gmail.com>
2022-02-08 22:18:53 +0100

    render next race data to html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let previousRace = document.getElementById("previous");
+let nextRace = document.getElementById("next");
 
-window.addEventListener("load", fetchLastRace);
+window.addEventListener("load", loadEvents);
 
 function fetchLastRace() {
     fetch("http://ergast.com/api/f1/current/last.json")
@@ -14,21 +15,58 @@ function fetchLastRace() {
             }
         })
         .then((data) => {
-            let previousTitle = "<h2>Last Race</h2>";
+            let divContent = "<h2>Last Race</h2>";
             let raceName = data["MRData"]["RaceTable"]["Races"][0]["raceName"];
             let raceCircuit = data["MRData"]["RaceTable"]["Races"][0]["Circuit"]["circuitName"];
             let raceDate = data["MRData"]["RaceTable"]["Races"][0]["date"];
             let raceStart = data["MRData"]["RaceTable"]["Races"][0]["time"];
-            previousTitle += `
+            divContent += `
                 <p>${raceName}</p>
                 <p>${raceCircuit}</p>
                 <p>${raceDate}</p>
                 <p>${raceStart}</p>
             `;
-            previousRace.innerHTML = previousTitle;
+            previousRace.innerHTML = divContent;
             console.log(data["MRData"]["RaceTable"]["Races"][0])
         })
         .catch((error) => console.log(error));
+}
+
+
+function fetchNextRace() {
+    fetch("http://ergast.com/api/f1/current/next.json")
+        .then((response) => {
+            if (!response.ok) {
+                const error = new Error(response.status);
+                throw error;
+            }
+            else {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            let divContent = "<h2>Next Race</h2>";
+            let raceName = data["MRData"]["RaceTable"]["Races"][0]["raceName"];
+            let raceCircuit = data["MRData"]["RaceTable"]["Races"][0]["Circuit"]["circuitName"];
+            let raceDate = data["MRData"]["RaceTable"]["Races"][0]["date"];
+            let raceStart = data["MRData"]["RaceTable"]["Races"][0]["time"];
+            divContent += `
+                <p>${raceName}</p>
+                <p>${raceCircuit}</p>
+                <p>${raceDate}</p>
+                <p>${raceStart}</p>
+            `;
+            nextRace.innerHTML = divContent;
+            console.log(data["MRData"]["RaceTable"]["Races"][0])
+
+            //countdown here!!
+        })
+        .catch((error) => console.log(error));
+}
+
+function loadEvents() {
+    fetchLastRace();
+    fetchNextRace();
 }
 
 // Refine with promise all !!!
