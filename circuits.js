8dd512cf883952e56ab48d632244de9b552c35db
alternadiva <asomeonelse@gmail.com>
2022-02-22 23:04:05 +0100

    update missing circuit images

--- circuits.js
+++ circuits.js
@@ -1,95 +1,94 @@
-
 export let circuitInfo = [
-    {
-        circuitName: "Bahrain International Circuit",
-        circuitImg: "./assets/bahrain.png"
-    },
-    {
-        circuitName: "Jeddah Corniche Circuit",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Albert Park Grand Prix Circuit",
-        circuitImg: "./assets/melbourne.png"
-    },
-    {
-        circuitName: "Autodromo Enzo e Dino Ferrari",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Miami International Autodrome",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Circuit de Barcelona-Catalunya",
-        circuitImg: "./assets/barcelona.png"
-    },
-    {
-        circuitName: "Circuit de Monaco",
-        circuitImg: "./assets/monaco.png"
-    },
-    {
-        circuitName: "Baku City Circuit",
-        circuitImg: "./assets/baku.png"
-    },
-    {
-        circuitName: "Circuit Gilles Villeneuve",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Silverstone Circuit",
-        circuitImg: "./assets/silverstone.png"
-    },
-    {
-        circuitName: "Red Bull Ring",
-        circuitImg: "./assets/redbullring.png"
-    },
-    {
-        circuitName: "Circuit Paul Ricard",
-        circuitImg: "./assets/paul-ricard.png"
-    },
-    {
-        circuitName: "Hungaroring",
-        circuitImg: "./assets/hungaroring.png"
-    },
-    {
-        circuitName: "Circuit de Spa-Francorchamps",
-        circuitImg: "./assets/spa.png"
-    },
-    {
-        circuitName: "Circuit Park Zandvoort",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Autodromo Nazionale di Monza",
-        circuitImg: "./assets/monza.png"
-    },
-    {
-        circuitName: "Sochi Autodrom",
-        circuitImg: "./assets/sochi.png"
-    },
-    {
-        circuitName: "Marina Bay Street Circuit",
-        circuitImg: "./assets/singapore.png"
-    },
-    {
-        circuitName: "Suzuka Circuit",
-        circuitImg: "./assets/suzuka.png"
-    },
-    {
-        circuitName: "Circuit of the Americas",
-        circuitImg: "./assets/americas.png"
-    },
-    {
-        circuitName: "Autódromo Hermanos Rodríguez",
-        circuitImg: "./assets/no-img.png"
-    },
-    {
-        circuitName: "Autódromo José Carlos Pace",
-        circuitImg: "./assets/brazil.png"
-    },
-    {
-        circuitName: "Yas Marina Circuit",
-        circuitImg: "./assets/abudhabi.png"
-    }
-]
+  {
+    circuitName: "Bahrain International Circuit",
+    circuitImg: "./assets/bahrain.png",
+  },
+  {
+    circuitName: "Jeddah Corniche Circuit",
+    circuitImg: "./assets/jeddah.png",
+  },
+  {
+    circuitName: "Albert Park Grand Prix Circuit",
+    circuitImg: "./assets/melbourne.png",
+  },
+  {
+    circuitName: "Autodromo Enzo e Dino Ferrari",
+    circuitImg: "./assets/imola.png",
+  },
+  {
+    circuitName: "Miami International Autodrome",
+    circuitImg: "./assets/no-img.png",
+  },
+  {
+    circuitName: "Circuit de Barcelona-Catalunya",
+    circuitImg: "./assets/barcelona.png",
+  },
+  {
+    circuitName: "Circuit de Monaco",
+    circuitImg: "./assets/monaco.png",
+  },
+  {
+    circuitName: "Baku City Circuit",
+    circuitImg: "./assets/baku.png",
+  },
+  {
+    circuitName: "Circuit Gilles Villeneuve",
+    circuitImg: "./assets/canada.png",
+  },
+  {
+    circuitName: "Silverstone Circuit",
+    circuitImg: "./assets/silverstone.png",
+  },
+  {
+    circuitName: "Red Bull Ring",
+    circuitImg: "./assets/spielberg.png",
+  },
+  {
+    circuitName: "Circuit Paul Ricard",
+    circuitImg: "./assets/paul-ricard.png",
+  },
+  {
+    circuitName: "Hungaroring",
+    circuitImg: "./assets/hungaroring.png",
+  },
+  {
+    circuitName: "Circuit de Spa-Francorchamps",
+    circuitImg: "./assets/spa.png",
+  },
+  {
+    circuitName: "Circuit Park Zandvoort",
+    circuitImg: "./assets/zandvoort.png",
+  },
+  {
+    circuitName: "Autodromo Nazionale di Monza",
+    circuitImg: "./assets/monza.png",
+  },
+  {
+    circuitName: "Sochi Autodrom",
+    circuitImg: "./assets/sochi.png",
+  },
+  {
+    circuitName: "Marina Bay Street Circuit",
+    circuitImg: "./assets/singapore.png",
+  },
+  {
+    circuitName: "Suzuka Circuit",
+    circuitImg: "./assets/suzuka.png",
+  },
+  {
+    circuitName: "Circuit of the Americas",
+    circuitImg: "./assets/americas.png",
+  },
+  {
+    circuitName: "Autódromo Hermanos Rodríguez",
+    circuitImg: "./assets/mexico.png",
+  },
+  {
+    circuitName: "Autódromo José Carlos Pace",
+    circuitImg: "./assets/brazil.png",
+  },
+  {
+    circuitName: "Yas Marina Circuit",
+    circuitImg: "./assets/abudhabi.png",
+  },
+];
